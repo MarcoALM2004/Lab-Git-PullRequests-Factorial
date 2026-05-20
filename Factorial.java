cc4b46dfb3eae2f94e1745b0ec3f772c3127ffc3
MarcoALM2004 <u23258586@utp.edu.pe>
2026-05-19 19:30:47 -0500

    feat: implementar factorial funcional con LongStream

--- Factorial.java
+++ Factorial.java
@@ -1,9 +1,9 @@
-// V2: Implementación recursiva
+import java.util.stream.LongStream;
+// V3: Implementación funcionalv
 public class Factorial {
  public static long calcular(int n) {
- if (n <= 1) {
- return 1;
- }
- return n * calcular(n - 1);
+ return LongStream
+ .rangeClosed(1, n)
+ .reduce(1, (a, b) -> a * b);
  }
 }
